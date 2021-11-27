d4544751c5526f9003fc92aa05489b7fcd9cb138
jeff <tangj1122@gmail.com>
2021-11-27 09:04:51 -0500

    update filenames

--- sidebars.js
+++ sidebars.js
@@ -26,8 +26,7 @@ const sidebars = {
       items: [
         'user_guide/installation',
         'user_guide/basics',
-        'user_guide/features',
-        'user_guide/all',
+        'user_guide/feature-reference',
       ],
     },
     {
